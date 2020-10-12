56766adb8ea429eb461f3838639a4a8e555d5691
Aaron Cleveland <toscleveland@gmail.com>
2020-10-11 23:24:27 -0500

    Added tabbar.

--- Nomad/MainTabController.swift
+++ Nomad/MainTabController.swift
@@ -16,13 +16,13 @@ class MainTabController: UITabBarController {
     
     func configureViewControllers() {
         let resources = ViewController()
-        let nav1 = templateNavigationController(image: UIImage(named: "person"), rootViewController: resources)
+        let nav1 = templateNavigationController(image: UIImage(systemName: "bolt"), rootViewController: resources)
         
         let explore = GeoMapViewController()
-        let nav2 = templateNavigationController(image: UIImage(named: "person"), rootViewController: explore)
+        let nav2 = templateNavigationController(image: UIImage(systemName: "map"), rootViewController: explore)
         
         let settings = SettingsViewController()
-        let nav3  = templateNavigationController(image: UIImage(named: "person"), rootViewController: settings)
+        let nav3  = templateNavigationController(image: UIImage(systemName: "gear"), rootViewController: settings)
         
         viewControllers = [nav1, nav2, nav3]
     }
@@ -30,6 +30,7 @@ class MainTabController: UITabBarController {
     func templateNavigationController(image: UIImage?, rootViewController: UIViewController) -> UINavigationController {
         let nav = UINavigationController(rootViewController: rootViewController)
         nav.navigationBar.isHidden = true
+        nav.tabBarItem.image = image
         return nav
     }
 }
--- Nomad/SettingsViewController.swift
+++ Nomad/SettingsViewController.swift
@@ -8,9 +8,68 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
+    
+    let detailVStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.alignment = .center
+        stack.distribution = .equalSpacing
+        return stack
+    }()
+    
+    let rankImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFit
+        image.image = UIImage(named: "private")
+        image.widthAnchor.constraint(equalToConstant: 75).isActive = true
+        image.heightAnchor.constraint(equalToConstant: 75).isActive = true
+        return image
+    }()
+    
+    let profileName: UILabel = {
+        let name = UILabel()
+        name.translatesAutoresizingMaskIntoConstraints = false
+        name.text = "General Tots"
+        return name
+    }()
+    
+    let troopCount: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Troops: 100"
+        return label
+    }()
+    
+    let intelligenceCount: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Intelligence: 100"
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .white
+        subviews()
+        constraints()
+    }
+}
+
+extension SettingsViewController {
+    private func subviews() {
+        detailVStack.addArrangedSubview(rankImage)
+        detailVStack.addArrangedSubview(profileName)
+        detailVStack.addArrangedSubview(troopCount)
+        detailVStack.addArrangedSubview(intelligenceCount)
+        view.addSubview(detailVStack)
+    }
+    
+    private func constraints() {
+        detailVStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        detailVStack.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        detailVStack.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        detailVStack.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
     }
 }
